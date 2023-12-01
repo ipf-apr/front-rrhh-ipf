d5733cd9f61e2b81a73b9b18544b8595de2e4b16
alexsh882 <alexsh882@gmail.com>
2023-12-01 15:57:59 -0300

    FEAT: Se agregaron cantidad de empleados por género en la vista dashboard.

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,22 +1,58 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 import { Spinner } from "../../components/Spinner";
 
 import { usePromise } from "../../hooks/usePromise";
 import { fetchDashboard } from "../../services/local/dashboard";
 import { formatDate } from "../../helpers/formatDate";
+import { showGender } from "../../helpers/showGender";
 
 export const Dashboard = () => {
   const { data, error, loading } = usePromise(fetchDashboard);
 
+  const navigate = useNavigate();
+
   console.log(data);
+
+  const handleNavigateToEmployeesByGender = (url) => () => {
+    console.log(url);
+    navigate(url);
+  };
 
   return (
     <div className="container-fluid py-5 px-md-5 col">
       <header className="d-flex align-items-center justify-content-between">
         <h1>Página de inicio</h1>
       </header>
-      <main className="col h-100">
+      <main className="row h-100">
+        <div className="col-md-6">
+          <div className="card">
+            <div className="card-header d-flex align-items-center gap-3 ">
+              <h3 className="card-title">Empleados por género </h3>
+            </div>
+            <div className="card-body p-2">
+              <ul className="list-unstyled d-flex flex-wrap justify-content-evenly  gap-2">
+                {!loading &&
+                  data.employeesByGender?.length > 0 &&
+                  data.employeesByGender.map((employee) => {
+                    return (
+                      <li
+                        key={`employee-by-gender-${employee.gender}`}
+                        style={{ cursor: "pointer" }}
+                        className="text-center border rounded-2 p-3 col-3 "
+                        onClick={handleNavigateToEmployeesByGender(
+                          `/employees?gender=${employee.gender}`
+                        )}
+                      >
+                        <p className="fs-1 fw-bold">{employee.count}</p>
+                        <span>{showGender(employee.gender)}s</span>
+                      </li>
+                    );
+                  })}
+              </ul>
+            </div>
+          </div>
+        </div>
         <div className="col-md-6">
           {loading ? (
             <div className="d-flex justify-content-center w-100 ">
@@ -60,7 +96,6 @@ export const Dashboard = () => {
                     })}
                 </ul>
               </div>
-
               <Link
                 className="card-footer text-center text-dark text-decoration-none"
                 to="/employees"
